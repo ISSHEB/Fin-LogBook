e23fdcf64e5444ad265257b409973ea17a005a41
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2023-08-29 01:07:33 +0300

    123124

--- src/App.js
+++ src/App.js
@@ -9,97 +9,78 @@ import { NoShowComponent } from './components/NoShowComponent'
 import { PdComponent } from './components/PdComponent'
 import { PmComponent } from './components/PmComponent'
 import { MiniIfoComponent } from './components/MiniIfoComponent'
-import { Setings } from './components/Settings';
+import { SettingsPage } from './components/Settings';
 import { Help } from './components/Help'
-// import { socketInitializer } from './helpers/socket';
 
+function App() 
+{
+    var [PageData, setPageData] = useState('/');
+    var ActionPages             = {
+        '/': <ControlPaymant 
 
-function App() {
-  var [PageData, setPageData] = useState('/');
-  // var [socket, setSocket] = useState(false);
-  // var [SocketConnected, setSocketConnected] = useState(false);
+        />,
+        '/info': <Information 
+            
+        />,
+        '/noShow': <NoShowComponent 
+            
+        />,
+        '/pd': <PdComponent 
+            
+        />,
+        '/pm': <PmComponent 
+            
+        />,
+        '/Miniinfo': <MiniIfoComponent 
+            
+        />,
+        '/Setings': <SettingsPage 
+            
+        />,
+        '/help': <Help 
+            
+        />,
+    };
 
-  // useEffect(() => {
-  //   (async () => {
-  //     setSocket(await socketInitializer());
-  //   })()
-  // }, []);
+    return (
+        <main className='App'>
+            <Sidebar>
+                <span onClick={(() => setPageData('/'))} >
+                    <SidebarItem icon={<Receipt size={20} />}
+                        text="Контроль оплат"
+                    />
+                </span>
+                <span onClick={(() => setPageData('/info'))}>
+                    <SidebarItem icon={<LayoutDashboard Receipt size={20} />} text="Информации" />
+                </span>
+                <span onClick={(() => setPageData('/noShow'))}>
+                    <SidebarItem icon={<UserCircle size={20} />} text="No show" />
+                </span>
+                <span onClick={(() => setPageData('/pd'))}>
+                    <SidebarItem icon={<Boxes size={20} />} text="Pd" />
+                </span>
+                <span onClick={(() => setPageData('/pm'))}>
+                    <SidebarItem icon={<Boxes size={20} />} text="Pm" />
+                </span>
+                <span onClick={(() => setPageData('/Miniinfo'))}>
+                    <SidebarItem icon={<Info size={20} />} text="Дополнительные информации" /> <hr className="my-3" />
+                </span>
+                <span onClick={(() => setPageData('/Setings'))}>
+                    <SidebarItem icon={<Settings size={20} />} text="Settings" />
+                </span>
+                <span onClick={(() => setPageData('/help'))}>
+                    <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+                </span>
+            </Sidebar>
 
-  // useEffect(() => {
-  //   (async () => {
-  //     if (socket) {
-  //       socket.on('connect', () => {
-  //         setSocketConnected(true);
-  //       });
+            <div className='body'>
+                {
+                    ActionPages[PageData]
+                }
+            </div>
+        </main>
 
-  //       socket.on('disconnect', () => {
-  //         setSocketConnected(false);
-  //       });
-  //     };
-  //   })()
-  // }, [socket]);
-
-  return (
-    <main className='App'>
-      <Sidebar>
-        <span onClick={(() => setPageData('/'))} >
-          <SidebarItem icon={<Receipt size={20} />}
-            text="Контроль оплат"
-          />
-        </span>
-        <span onClick={(() => setPageData('/info'))}>
-          <SidebarItem icon={<LayoutDashboard Receipt size={20} />} text="Информации" />
-        </span>
-        <span onClick={(() => setPageData('/noShow'))}>
-          <SidebarItem icon={<UserCircle size={20} />} text="No show" />
-        </span>
-        <span onClick={(() => setPageData('/pd'))}>
-          <SidebarItem icon={<Boxes size={20} />} text="Pd" />
-        </span>
-        <span onClick={(() => setPageData('/pm'))}>
-          <SidebarItem icon={<Boxes size={20} />} text="Pm" />
-        </span>
-        <span onClick={(() => setPageData('/Miniinfo'))}>
-          <SidebarItem icon={<Info size={20} />} text="Дополнительные информации" /> <hr className="my-3" />
-        </span>
-        <span onClick={(() => setPageData('/Setings'))}>
-          <SidebarItem icon={<Settings size={20} />} text="Settings" />
-        </span>
-        <span onClick={(() => setPageData('/help'))}>
-          <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
-        </span>
-
-      </Sidebar>
-      <div className='body'>
-        {
-          PageData === '/' ? (
-            <ControlPaymant />
-          ) : PageData === '/info' ? (
-            <Information />
-
-          ) : PageData === '/noShow' ? (
-            <NoShowComponent />
-
-          ) : PageData === '/pd' ? (
-            <PdComponent />
-
-          ) : PageData === '/pm' ? (
-            <PmComponent />
-
-          ) : PageData === '/Miniinfo' ? (
-            <MiniIfoComponent />
-          )
-            : PageData === '/Setings' ? (
-              <Settings />
-
-            ) : PageData === '/help' ? (
-              <Help />
-            ) : null
-        }
-      </div>
-    </main>
-
-  );
+    );
 }
 
 export default App;
--- src/components/Todo/InformationTodo.js
+++ src/components/Todo/InformationTodo.js
@@ -2,57 +2,62 @@ import React, { useEffect, useState } from 'react';
 import { Trash2, Pencil } from "lucide-react";
 
 export default function InformationTodo() {
-	const [FormInp, setFormInp] = useState("");
-	const [FormInp2, setFormInp2] = useState("");
-	const [todoInfo, setTodoInfo] = useState([]);
 
-	async function createNewTodo() {
-		var newTodoInfo = todoInfo;
+    const [todoInfo, setTodoInfo]       = useState([]);
+    const [test, setTest]               = useState(null);
+    const [FormInp, setFormInp]         = useState("");
+	const [FormInp2, setFormInp2]       = useState("");
+    
+    function setNewData()
+    {
+        var newTodoInfo = [];
 
-		newTodoInfo.push({
+        for(var element of todoInfo)
+        {
+            newTodoInfo.push(element);
+        };
+
+        newTodoInfo.push({
 			roomNumber: FormInp,
 			comment: FormInp2,
-			data: new Date().getTime().toString(),
 		});
 
-		await setTodoInfo(newTodoInfo);
-	};
+        setTodoInfo(newTodoInfo);
+    };
 
-	useEffect(() => {
-		console.log(todoInfo);
-	}, [todoInfo])
-
-	return (
-		<div className="blockInfo">
-			<div className="blockInfoHeader">
-				<h1>Информации</h1>
-			</div>
-			<div className="bodyInfo">
-				<div>
-					{todoInfo.map((item) => (
-						<div className="boxInfo" key={item.roomNumber}>
-							<div className="boxHeader">
-								<h6>
-									Комнатa: <b className="boxHeaderText">{item.roomNumber}</b>
-								</h6>
-							</div>
-							<div className="boxBodyInfo">{item.comment}</div>
-							<div className="boxEnd">
-								<p>{(() => new Date(item.date).customFormat("#DD#/#MM#/#YYYY#"))}</p>
-								<div className="boxEndIcon">
-									<Pencil size={20} className="icon" />
-									<Trash2 size={20} className="icon" />
-								</div>
-							</div>
-						</div>
-					))}
-				</div>
-				<aside className="form">
-					<input onChange={((e) => setFormInp(e.target.value))} className="formInp" placeholder="Номер комнаты" />
-					<textarea onChange={((e) => setFormInp2(e.target.value))} className="formInp2" placeholder="Информация"></textarea>
-					<button onClick={(() => createNewTodo())} className="infoButton">Добавить</button>
-				</aside>
-			</div>
-		</div>
-	);
+    return(
+        <>
+            <div className="blockInfo">
+                <div className="blockInfoHeader">
+                    <h1>Информации</h1>
+                </div>
+                <div className="bodyInfo">
+                    <div>
+                        {todoInfo.map((item) => (
+                            <div className="boxInfo" key={item.roomNumber}>
+                                <div className="boxHeader">
+                                    <h6>
+                                        Комнатa: <b className="boxHeaderText">{item.roomNumber}</b>
+                                    </h6>
+                                </div>
+                                <div className="boxBodyInfo">{item.comment}</div>
+                                <div className="boxEnd">
+                                    <p>{(() => new Date(item.date).customFormat("#DD#/#MM#/#YYYY#"))}</p>
+                                    <div className="boxEndIcon">
+                                        <Pencil size={20} className="icon" />
+                                        <Trash2 size={20} className="icon" />
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <aside className="form">
+                        <input onChange={((e) => setFormInp(e.target.value))} className="formInp" placeholder="Номер комнаты" />
+                        <textarea onChange={((e) => setFormInp2(e.target.value))} className="formInp2" placeholder="Информация"></textarea>
+                        <button onClick={setNewData} className="infoButton">Добавить</button>
+                    </aside>
+                </div>
+            </div>
+        </>
+    );
 };
